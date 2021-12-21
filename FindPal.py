1968423f0ac29c5b220fd20c1ce91156518d43e0
GYepesB <gyepesb@alumnos.nebrija.es>
2021-12-21 18:01:27 +0100

    Initial commit

---
+++ FindPal.py
@@ -0,0 +1,17 @@
+class FindPal:
+    def __init__(self, PA, FA, archivo, palabras, frases):
+       self.PA = PA
+       self.FA = FA
+       self.archivo = archivo
+       self.palabras = palabras
+       self.frases = frases
+
+
+
+
+
+
+
+
+
+
